0a741a68a5bf5b537c9c69425d8faedc1748c8bb
Nest <nsitseo@gmail.com>
2023-04-28 12:32:38 +0530

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app';
+
+import Layout from '@/components/Layout';
+import '@/styles/globals.css';
+import Modal from '@/components/Modal';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Modal isOpen title="Test Modal" />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+}
